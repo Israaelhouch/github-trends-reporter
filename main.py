ec969dbc271ccfb428b9c83596e01cbfc1c6ced6
Israa Elhouch <issrach235@gmail.com>
2025-10-13 14:52:04 +0100

    refactor: fiw some params

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from src.fetcher.fetching_pipline import run_fetch
 from src.processor.processing_pipline import run_preprocess
+from src.notifier.notifying_pipline import run_notifier
 from src.utils.logger import setup_logger
 
 logger = setup_logger()
 
 def main():
     logger.info("Starting GitHub Trends Reporter pipeline...")
-
-    trends = run_fetch("machine learning", 200)
-    run_preprocess(topic="machine learning")
+    topic="machine learning"
+    trends = run_fetch(topic, 200)
+    run_preprocess(topic=topic)
+    run_notifier(topic=topic)
 
     logger.info("Pipeline finished successfully.")
 
