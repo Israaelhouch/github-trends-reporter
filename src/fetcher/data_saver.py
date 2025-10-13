690a1f0b26bca7f5675b47c8ec6017f7a87b6444
Israa Elhouch <issrach235@gmail.com>
2025-10-13 13:15:18 +0100

    fix(fetcher): integrate github_api with config.py and correct pagination

--- src/fetcher/data_saver.py
+++ src/fetcher/data_saver.py
@@ -1,21 +1,15 @@
 import os
 import csv
-import json
-from config import DATA_RAW_DIR, DATA_OUTPUT_DIR
-from utils.logger import setup_logger
+from config import DATA_RAW_DIR
+from src.utils.logger import setup_logger
 from datetime import datetime
 
 logger = setup_logger("fetcher")
 
 os.makedirs(DATA_RAW_DIR, exist_ok=True)
-os.makedirs(DATA_OUTPUT_DIR, exist_ok=True)
 
-def save_json(data, name: str):
-    """Save analysis or raw data as JSON"""
-    if not data:
-        logger.warning("No data to save in JSON.")
-        return
-    filepath = os.path.join(DATA_OUTPUT_DIR, f"{name}_{datetime.utcnow().strftime('%Y_%m_%d')}.json")
-    with open(filepath, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
-    logger.info(f"Saved JSON file: {filepath}")
+def save_raw_csv(df, topic: str):
+    """Save list of repos as CSV in raw folder"""
+    filename = f"{DATA_RAW_DIR}/github_trends_{topic.replace(' ', '_')}_{datetime.utcnow().strftime('%Y_%m_%d')}.csv"
+    df.to_csv(filename, index=False)
+    logger.info(f"Saved {len(df)} repositories to '{filename}'")
